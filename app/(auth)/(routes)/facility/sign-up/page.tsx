56863fdbace1e96bf28d4c385713673b25949e64
Les Chinyanga <les.chinyanga@africacode.academy>
2023-10-28 05:34:57 +0200

    feat: add steps to the onboarding page

--- app/(auth)/(routes)/facility/sign-up/page.tsx
+++ app/(auth)/(routes)/facility/sign-up/page.tsx
@@ -4,6 +4,9 @@ import useFacilityAuth from '@/app/context/AuthContext/hook';
 
 import BasicFacilityInfo from '@/components/auth/FacilityRegisterFlow/step-1';
 import FacilityLocation from '@/components/auth/FacilityRegisterFlow/step-2';
+import CreatePasswordFacility from '@/components/auth/FacilityRegisterFlow/step-3';
+import UploadFacilityDoc from '@/components/auth/FacilityRegisterFlow/step-4';
+import FinalFacilityOnboardingStep from '@/components/auth/FacilityRegisterFlow/step-5';
 import RegisterCard from '@/components/auth/register-card';
 import React from 'react';
 
@@ -13,6 +16,12 @@ const RegisterFacilitySignUp = () => {
   if (currentStep === 0) return <BasicFacilityInfo />;
 
   if (currentStep === 1) return <FacilityLocation />;
+
+  if (currentStep === 2) return <CreatePasswordFacility />;
+
+  if (currentStep === 3) return <UploadFacilityDoc />;
+
+  if (currentStep === 4) return <FinalFacilityOnboardingStep />;
 
   return (
     <div>
