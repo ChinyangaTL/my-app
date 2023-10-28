621dcc4c63288e3f446d1150830345bbfa3175a1
Les Chinyanga <les.chinyanga@africacode.academy>
2023-10-28 04:05:31 +0200

    feat: update the formstate to include the district

--- components/auth/FacilityRegisterFlow/step-2.tsx
+++ components/auth/FacilityRegisterFlow/step-2.tsx
@@ -1,9 +1,23 @@
 'use client';
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Button } from '@/components/ui/button';
+
 import React, { useState } from 'react';
 import { useJsApiLoader, Autocomplete } from '@react-google-maps/api';
+import useFacilityAuth from '@/app/context/AuthContext/hook';
+import { set } from 'zod';
 
 const FacilityLocation = () => {
+  const { setCurrentStep, setFormState } = useFacilityAuth();
+
+  const [district, setDistrict] = useState('South East');
   const [searchResult, setSearchResult] = useState('Result: none');
   const [facililityLocationInfo, setFacilityLocationInfo] = useState<{
     name: '';
@@ -23,6 +37,23 @@ const FacilityLocation = () => {
   if (!isLoaded) return <div>Loading...</div>;
   return (
     <div className='w-100% bg-red-100'>
+      <Select onValueChange={(value) => setDistrict(value)}>
+        <SelectTrigger className='w-[180px]'>
+          <SelectValue placeholder='South East' />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value='CEN'>Central</SelectItem>
+          <SelectItem value='CHO'>Chobe</SelectItem>
+          <SelectItem value='GHA'>Ghanzi</SelectItem>
+          <SelectItem value='KGA'>Kgalagadi</SelectItem>
+          <SelectItem value='KGT'>Kgatleng</SelectItem>
+          <SelectItem value='KWE'>Kweneng</SelectItem>
+          <SelectItem value='NE'>North East</SelectItem>
+          <SelectItem value='NW'>North West</SelectItem>
+          <SelectItem value='SE'>South East</SelectItem>
+          <SelectItem value='SOU'>Southern</SelectItem>
+        </SelectContent>
+      </Select>
       <Autocomplete
         onLoad={onLoad}
         restrictions={{ country: 'bw' }}
@@ -34,7 +65,7 @@ const FacilityLocation = () => {
             const formattedAddress = place.formatted_address;
             console.log(formattedAddress);
 
-            // console.log(place);
+            console.log(place);
             setFacilityLocationInfo({
               name: place.name,
               businessStatus: place.business_status,
@@ -55,6 +86,19 @@ const FacilityLocation = () => {
           <p>{facililityLocationInfo.formattedAddress}</p>
         </div>
       )}
+
+      <Button
+        onClick={() => {
+          setFormState((prev) => ({
+            ...prev,
+            address: facililityLocationInfo?.formattedAddress,
+            district,
+          }));
+          setCurrentStep(2);
+        }}
+      >
+        Next
+      </Button>
     </div>
   );
 };
