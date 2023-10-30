b545534b5319ff14f528027fd11cd9b083c00571
Les Chinyanga <les.chinyanga@africacode.academy>
2023-10-30 18:11:46 +0200

    chore: code clean up

--- app/(facility)/(main)/dashboard/page.tsx
+++ app/(facility)/(main)/dashboard/page.tsx
@@ -1,38 +1,11 @@
 // 'use client';
 
-import useFacilityAuth from '@/app/context/AuthContext/hook';
-import { useAuth } from '@/app/hooks/useFacilityAuth';
 import { db } from '@/lib/db';
-import React from 'react';
 import { DataTable } from './data-table';
-import { Payment, columns } from './columns';
-import { useQuery } from '@tanstack/react-query';
-import { signOut, useSession } from 'next-auth/react';
-import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
+import { columns } from './columns';
+import { signOut } from 'next-auth/react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-
-const data = [
-  {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '728ed52g',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '728ed52h',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-];
 
 const Dashboard = async () => {
   // const drugs = await db.
@@ -78,6 +51,7 @@ const Dashboard = async () => {
   return (
     <>
       <p>you are logged in as {session?.user.id}</p>
+
       <DataTable columns={columns} data={facilityDrugs} />
       {/* <Button onClick={logout}>sign out</Button> */}
     </>
