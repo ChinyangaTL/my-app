eb7e31d7f9ca7fcd6979eeee91d7ef961744bba3
Les Chinyanga <les.chinyanga@africacode.academy>
2023-10-30 18:31:07 +0200

    fix: remove facilty context reference from user page

--- app/(user)/user/page.tsx
+++ app/(user)/user/page.tsx
@@ -4,9 +4,6 @@ import useFacilityAuth from '@/app/context/AuthContext/hook';
 import React from 'react';
 
 const User = () => {
-  const { currentStep } = useFacilityAuth();
-
-  console.log(currentStep);
   return <div>page</div>;
 };
 
